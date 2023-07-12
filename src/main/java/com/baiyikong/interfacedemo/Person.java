bae062da6648e18bdfeb29d0a7a8a6b0e95e39d5
baiyikong <baiyikong@foxmail.com>
2023-07-13 01:49:42 +0800

    练习一下接口，导包

---
+++ src/main/java/com/baiyikong/interfacedemo/Person.java
@@ -0,0 +1,13 @@
+package com.baiyikong.interfacedemo;
+
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Person {
+    private String name;
+    private int age;
+
+}
---
+++ src/main/java/com/baiyikong/interfacedemo/Test.java
@@ -0,0 +1,11 @@
+package com.baiyikong.interfacedemo;
+
+public class Test {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.setName("小羊");
+        p.setAge(20);
+
+        System.out.println(p.getName());
+    }
+}
