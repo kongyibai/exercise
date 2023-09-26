397c9ac4409d37a20db547dee276bfea91cf2459
陈品艺 <baiyikong@foxmail.com>
2023-09-26 09:28:39 +0800

    测试一下exercise的提交

---
+++ src/main/java/com/baiyikong/containerdemo/ContainerDemo1.java
@@ -0,0 +1,10 @@
+package com.baiyikong.containerdemo;
+
+public class ContainerDemo1 {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+        System.out.println("hello,world");
+    }
+}
